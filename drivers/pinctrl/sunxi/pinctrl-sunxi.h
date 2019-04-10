d95f3a3b06998a431a1fd341dd1dac6ebd4bca56
Ondrej Jirman <megous@megous.com>
2019-04-11 00:06:12 +0200

    pinctrl: sunxi: Support I/O bias voltage setting on H6
    H6 SoC has a "pio group withstand voltage mode" register (datasheet
    description), that needs to be used to select either 1.8V or 3.3V I/O mode,
    based on what voltage is powering the respective pin banks and is thus used
    for I/O signals.
    
    Add support for configuring this register according to the voltage of the
    pin bank regulator (if enabled).
    
    This is similar to the support for I/O bias voltage setting patch for A80
    and the same concerns apply. See:
    
      commit 402bfb3c1352 ("Support I/O bias voltage setting on A80")
    
    Signed-off-by: Ondrej Jirman <megous@megous.com>
    
    # Conflicts:
    #	drivers/pinctrl/sunxi/pinctrl-sunxi.c

--- drivers/pinctrl/sunxi/pinctrl-sunxi.h
+++ drivers/pinctrl/sunxi/pinctrl-sunxi.h
@@ -95,11 +95,16 @@
 #define PINCTRL_SUN7I_A20	BIT(7)
 #define PINCTRL_SUN8I_R40	BIT(8)
 
+#define PIO_POW_MOD_SEL_REG	0x340
+
 enum sunxi_desc_bias_voltage {
 	BIAS_VOLTAGE_NONE,
 	/* Bias voltage configuration is done through
 	 * Pn_GRP_CONFIG registers, as seen on A80 SoC. */
 	BIAS_VOLTAGE_GRP_CONFIG,
+	/* Bias voltage is set through PIO_POW_MOD_SEL_REG
+	 * register, as seen on H6 SoC, for example. */
+	BIAS_VOLTAGE_PIO_POW_MODE_SEL,
 };
 
 struct sunxi_desc_function {
