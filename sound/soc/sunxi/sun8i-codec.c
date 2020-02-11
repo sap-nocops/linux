5c411d7e3688d1e4206672118f6f94c278a45c68
Ondrej Jirman <megous@megous.com>
2020-02-11 14:33:08 +0100

    ac100-codec: Drop debug statements
    Everything seems to work. :)
    
    Signed-off-by: Ondrej Jirman <megous@megous.com>

--- sound/soc/sunxi/sun8i-codec.c
+++ sound/soc/sunxi/sun8i-codec.c
@@ -1116,20 +1116,13 @@ static struct regmap_config sun8i_codec_regmap_config = {
 static int sun8i_codec_ac100_regmap_read(void *context, unsigned int reg, unsigned int *val)
 {
 	struct sun8i_codec *scodec = context;
-	int ret;
 
-	ret = regmap_read(scodec->ac100_regmap, reg / 4, val);
-	if (ret == 0)
-		pr_err("R%02x => %04x\n", reg / 4, *val);
-
-	return ret;
+	return regmap_read(scodec->ac100_regmap, reg / 4, val);
 }
 
 static int sun8i_codec_ac100_regmap_write(void *context, unsigned int reg, unsigned int val)
 {
 	struct sun8i_codec *scodec = context;
-
-	pr_err("W%02x <= %04x\n", reg / 4, val);
 
 	return regmap_write(scodec->ac100_regmap, reg / 4, val);
 }
